363baa59b6ee8b817898164d886c2c6dde797864
danicaicedo <danielhumbertocaicedoarguello@gmail.com>
2025-10-06 21:20:40 -0500

    Proyecto Android reorganizado en la raíz para compilación

---
+++ Desktop/finalmente/finalmente/app/src/main/java/menu/AtragantamientoActivity.kt
@@ -0,0 +1,65 @@
+package com.aristidevs.finalmente.menu
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.aristidevs.finalmente.R
+import com.aristidevs.finalmente.menu.MenuActivity
+
+
+class AtragantamientoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_atragantamiento)
+
+        val btnBack = findViewById<ImageButton>(R.id.btnBack)
+        btnBack.setOnClickListener {
+            // Cierra esta actividad y vuelve a la anterior
+            finish()
+        }
+
+        // === BOTÓN HOME: ir al menú principal ===
+        val btnHome = findViewById<LinearLayout>(R.id.homeButtonLayout)
+        btnHome.setOnClickListener {
+            val intent = Intent(this, MenuActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
+            finish() // Opcional: cerrar esta pantalla
+        }
+
+        val paso1 = findViewById<LinearLayout>(R.id.paso1)
+        val paso1Detalle = findViewById<TextView>(R.id.step1Detalle)
+        paso1.setOnClickListener {
+            paso1Detalle.visibility = if (paso1Detalle.visibility == View.GONE) View.VISIBLE else View.GONE
+        }
+
+        val paso2 = findViewById<LinearLayout>(R.id.paso2)
+        val paso2Detalle = findViewById<TextView>(R.id.step2Detalle)
+        paso2.setOnClickListener {
+            paso2Detalle.visibility = if (paso2Detalle.visibility == View.GONE) View.VISIBLE else View.GONE
+        }
+
+        val paso3 = findViewById<LinearLayout>(R.id.paso3)
+        val paso3Detalle = findViewById<TextView>(R.id.step3Detalle)
+        paso3.setOnClickListener {
+            paso3Detalle.visibility = if (paso3Detalle.visibility == View.GONE) View.VISIBLE else View.GONE
+        }
+
+        val paso4 = findViewById<LinearLayout>(R.id.paso4)
+        val paso4Detalle = findViewById<TextView>(R.id.step4Detalle)
+        paso4.setOnClickListener {
+            paso4Detalle.visibility = if (paso4Detalle.visibility == View.GONE) View.VISIBLE else View.GONE
+        }
+
+        val paso5 = findViewById<LinearLayout>(R.id.paso5)
+        val paso5Detalle = findViewById<TextView>(R.id.step5Detalle)
+        paso5.setOnClickListener {
+            paso5Detalle.visibility = if (paso5Detalle.visibility == View.GONE) View.VISIBLE else View.GONE
+        }
+    }
+}
