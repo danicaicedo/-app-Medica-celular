3a331d2b91ae0c96a1326ed18bfcc68dafbeced7
danicaicedo <160039725+danicaicedo@users.noreply.github.com>
2025-10-07 21:05:14 -0500

    QuemaduraActivity.kt

--- app/src/main/java/menu/QuemaduraActivity.kt
+++ app/src/main/java/menu/QuemaduraActivity.kt
@@ -1,4 +1,12 @@
-package menu
+package com.aristidevs.finalmente.menu
 
-class QuemaduraActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.aristidevs.finalmente.R
+
+class QuemaduraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quemadura)
+    }
 }
